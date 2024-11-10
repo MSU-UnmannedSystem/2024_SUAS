1a11d5a9ce1be992be2339841b5e13c2d6103b60
andrewchiu1203 <81220384+andrewchiu1203@users.noreply.github.com>
2024-11-10 14:18:22 -0500

    Export model dependencies

--- img_recog/export_model.py
+++ img_recog/export_model.py
@@ -2,6 +2,9 @@ import ultralytics
 
 MODEL_PATH = ""
 
+# pip install ultralytics torch torchvision
+# pip install tensorflow onnx onnx-tf
+
 # Export PyTorch to NCNN
 model = ultralytics.YOLO(MODEL_PATH)
 model.export(format="ncnn")
