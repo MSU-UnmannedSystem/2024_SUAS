962ffd89b72493d2a74fd045550cb256505dea19
Kuan-Lun Chiu <msuus2024@outlook.com>
2025-01-19 13:47:12 -0500

    rename stuff

--- img_recog/check_cuda.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-cuda_is_availible = torch.cuda.is_available()
-print()
-print("Cuda device found:\n\t", cuda_is_availible)
-
-if cuda_is_availible:
-    print("Device name:\n\t", torch.cuda.get_device_name())
-    print("Cuda version:\n\t", torch.version.cuda)
---
+++ img_recog/inference.py
@@ -0,0 +1,78 @@
+import time
+import cv2
+from ultralytics import YOLO
+
+# Constant
+CONFIDENCE_THRESHOLD = 0.5
+RAW_WIDTH, RAW_HEIGHT = 1920, 1080
+FRAME_WIDTH, FRAME_HEIGHT = 640, 360
+
+# Common 16:9 resolutions
+""" 640 360
+    854 480
+    960 540
+    1024 57
+    1280 720
+    1366 768
+    1600 900
+    1920 1080
+    2560 1440
+    3200 1800
+    3840 2160
+    5120 2880
+    7680 4320 """
+
+def main():
+    # Load model
+    model = YOLO("model/yolov9t.pt", task = "detect")
+
+    # Setup webcam with openCV
+    cam = cv2.VideoCapture(0)
+    cam.set(cv2.CAP_PROP_FRAME_WIDTH, RAW_WIDTH)
+    cam.set(cv2.CAP_PROP_FRAME_HEIGHT, RAW_HEIGHT)
+
+    loop_counter, start_time, current_time, fps = 0, 0, 0, 0
+
+    while True:
+        if cv2.waitKey(1) == ord("q"):
+            break
+
+        _, frame = cam.read()
+        frame = cv2.resize(frame, (FRAME_WIDTH, FRAME_HEIGHT))
+        results = model(frame, verbose = False, conf = CONFIDENCE_THRESHOLD)
+
+        # Extract detection result from 1st object
+        for result in results[0].boxes:
+            class_id = int(result.cls)
+            class_label = results[0].names[class_id]
+            confidence = result.conf * 100
+            bbox = result.xyxy[0].tolist()
+            break
+            
+        # Show annotated frame with fps
+        annotated_frame = results[0].plot()
+        cv2.putText(img         = annotated_frame, 
+                    text        = "FPS: " + str(fps),
+                    org         = (25, 25),
+                    fontFace    = cv2.FONT_HERSHEY_SIMPLEX,
+                    fontScale   = 0.5,
+                    thickness   = 1,
+                    color       = (255, 0, 0))
+        
+        cv2.imshow("YOLOv9 inference result: press q to quit", annotated_frame)
+
+        # Calculate fps
+        current_time = time.time()
+        if (current_time - start_time) >= 1:
+            fps = loop_counter
+            start_time = current_time
+            loop_counter = 0
+
+        loop_counter += 1
+
+    # Close camera and openCV window
+    cam.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
