3f537746a369d762fcbf6c913e3d0ba1e3ca9c81
kuan <andrewchiu1203@gmail.com>
2025-02-14 08:48:09 -0500

    edit export script

--- img_recog/export.py
+++ img_recog/export.py
@@ -1,29 +1,21 @@
+'''
+    A x86 (non-ARM) platform running Linux is required for exporting.
+    The simpliest way is to use a Google Colab virtual runtime.
+'''
+
+# Run these to setup environment for exporting
+# pip uninstall tensorflow tensorflow-aarch64
+# pip install -U tflite-runtime
+# pip install ultralytics
+
+# Do these if ultralytics doesn't auto install all needed stuff
+# !pip install ultralytics torch torchvision
+# !pip install tflite-runtime onnx onnx-tf
+# !pip install tf_keras sng4onnx onnx_graphsurgeon onnx2tf
+# !pip install onnxslim tflite_support onnxruntime
+
 import ultralytics
 
-"""
-pip install ultralytics torch torchvision
-pip install tensorflow onnx onnx-tf
-pip install tf_keras sng4onnx onnx_graphsurgeon onnx2tf onnxslim tflite_support onnxruntime
-
-Visual Studio C++ >= 14
-Make sure to check the boxes for build tools and windows 11 SDK
-
-Coral should run TFLite and EdgeTPU but the latter performs better on their hardware
-Export EdgeTPU only works on x86-linux, so probably need some Google Colab environment
-
-Documentation -> https://docs.ultralytics.com/guides/coral-edge-tpu-on-raspberry-pi
-"""
-
-MODEL_PATH = "model/yolov9t.pt"
-
-# Export NCNN
-# model = ultralytics.YOLO(MODEL_PATH)
-# model.export(format="ncnn")
-
-# Export TensorFlow-Lite
-# model = ultralytics.YOLO(MODEL_PATH)
-# model.export(format="tflite")
-
 # Export EdgeTPU
-# model = ultralytics.YOLO(MODEL_PATH)
-# model.export(format="edgetpu")
+model = ultralytics.YOLO("path_to_pt_file")
+model.export(format="edgetpu")
